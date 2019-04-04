274b2faa09c667b4c9d2d29d4900ecdcecb22197
Christa Hatch <32205645+ChristaKH@users.noreply.github.com>
2019-04-04 15:07:51 -0800

    Finish app

--- app/src/main/java/edu/miracostacollege/cs134/touchgestures/MainActivity.java
+++ app/src/main/java/edu/miracostacollege/cs134/touchgestures/MainActivity.java
@@ -2,10 +2,16 @@ package edu.miracostacollege.cs134.touchgestures;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.View;
 import android.widget.ScrollView;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import static android.icu.lang.UProperty.MATH;
+
+public class MainActivity extends AppCompatActivity implements
+                GestureDetector.OnGestureListener, GestureDetector.OnDoubleTapListener {
 
     private ScrollView gesturesScrollView;
 
@@ -16,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
     private TextView scrollTextView;
     private TextView flingTextView;
 
+    private GestureDetector mGestureDetector;
     private int singleTaps = 0, doubleTaps = 0, longPresses = 0, scrolls = 0, flings = 0;
 
     @Override
@@ -23,6 +30,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        gesturesLogTextView = findViewById(R.id.gesturesLogTextView);
         gesturesScrollView = findViewById(R.id.gesturesScrollView);
         singleTapTextView = findViewById(R.id.singleTapTextView);
         doubleTapTextView = findViewById(R.id.doubleTapTextView);
@@ -30,7 +38,194 @@ public class MainActivity extends AppCompatActivity {
         scrollTextView = findViewById(R.id.scrollTextView);
         flingTextView = findViewById(R.id.flingTextView);
 
-
+        // Make a gesture detector to listen
+        mGestureDetector = new GestureDetector(gesturesScrollView.getContext(), this);
     }
 
+    /**
+     * Called to process touch screen events.  You can override this to
+     * intercept all touch screen events before they are dispatched to the
+     * window.  Be sure to call this implementation for touch screen events
+     * that should be handled normally.
+     *
+     * @param ev The touch screen event.
+     * @return boolean Return true if this event was consumed.
+     */
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        super.dispatchTouchEvent(ev);
+        return mGestureDetector.onTouchEvent(ev);
+    }
+
+    /**
+     * Notified when a single-tap occurs.
+     * <p>
+     * Unlike this
+     * will only be called after the detector is confident that the user's
+     * first tap is not followed by a second tap leading to a double-tap
+     * gesture.
+     *
+     * @param e The down motion event of the single-tap.
+     * @return true if the event is consumed, else false
+     */
+    @Override
+    public boolean onSingleTapConfirmed(MotionEvent e) {    // Press and release have been detected
+        gesturesLogTextView.append("\nonSingleTapConfirmed");
+        singleTaps++;
+        singleTapTextView.setText(singleTaps + "");
+        return true;
+    }
+
+    /**
+     * Notified when a double-tap occurs.
+     *
+     * @param e The down motion event of the first tap of the double-tap.
+     * @return true if the event is consumed, else false
+     */
+    @Override
+    public boolean onDoubleTap(MotionEvent e) {
+        gesturesLogTextView.append("\nonDoubleTap");
+        doubleTaps++;
+        doubleTapTextView.setText(doubleTaps +  "");
+        return true;
+    }
+
+    /**
+     * Notified when an event within a double-tap gesture occurs, including
+     * the down, move, and up events.
+     *
+     * @param e The motion event that occurred during the double-tap gesture.
+     * @return true if the event is consumed, else false
+     */
+    @Override
+    public boolean onDoubleTapEvent(MotionEvent e) {
+        gesturesLogTextView.append("\nonDoubleTapEvent");
+
+        return true;
+    }
+
+    /**
+     * Notified when a tap occurs with the down {@link MotionEvent}
+     * that triggered it. This will be triggered immediately for
+     * every down event. All other events should be preceded by this.
+     *
+     * @param e The down motion event.
+     */
+    @Override
+    public boolean onDown(MotionEvent e) {
+        gesturesLogTextView.append("\nonDown");
+        return true;
+    }
+
+    /**
+     * The user has performed a down {@link MotionEvent} and not performed
+     * a move or up yet. This event is commonly used to provide visual
+     * feedback to the user to let them know that their action has been
+     * recognized i.e. highlight an element.
+     *
+     * @param e The down motion event
+     */
+    @Override
+    public void onShowPress(MotionEvent e) {
+        gesturesLogTextView.append("\nonShowPress");
+    }
+
+    /**
+     * Notified when a tap occurs with the up {@link MotionEvent}
+     * that triggered it.
+     *
+     * @param e The up motion event that completed the first tap
+     * @return true if the event is consumed, else false
+     */
+    @Override
+    public boolean onSingleTapUp(MotionEvent e) {
+        gesturesLogTextView.append("\nonSingleTapUp");
+        return true;
+    }
+
+    /**
+     * Notified when a scroll occurs with the initial on down {@link MotionEvent} and the
+     * current move {@link MotionEvent}. The distance in x and y is also supplied for
+     * convenience.
+     *
+     * @param e1        The first down motion event that started the scrolling.
+     * @param e2        The move motion event that triggered the current onScroll.
+     * @param distanceX The distance along the X axis that has been scrolled since the last
+     *                  call to onScroll. This is NOT the distance between {@code e1}
+     *                  and {@code e2}.
+     * @param distanceY The distance along the Y axis that has been scrolled since the last
+     *                  call to onScroll. This is NOT the distance between {@code e1}
+     *                  and {@code e2}.
+     * @return true if the event is consumed, else false
+     */
+    @Override
+    public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+        gesturesLogTextView.append("\nonScroll: distanceX is " + distanceX + ", distanceY is " + distanceY);
+        scrolls++;
+        scrollTextView.setText(scrolls + "");
+        return true;
+    }
+
+    /**
+     * Notified when a long press occurs with the initial on down {@link MotionEvent}
+     * that trigged it.
+     *
+     * @param e The initial on down motion event that started the longpress.
+     */
+    @Override
+    public void onLongPress(MotionEvent e) {
+        gesturesLogTextView.append("\nonLongPress");
+        longPresses++;
+        longPressTextView.setText(longPresses + "");
+    }
+
+    /**
+     * Notified of a fling event when it occurs with the initial on down {@link MotionEvent}
+     * and the matching up {@link MotionEvent}. The calculated velocity is supplied along
+     * the x and y axis in pixels per second.
+     *
+     * @param e1        The first down motion event that started the fling.
+     * @param e2        The move motion event that triggered the current onFling.
+     * @param velocityX The velocity of this fling measured in pixels per second
+     *                  along the x axis.
+     * @param velocityY The velocity of this fling measured in pixels per second
+     *                  along the y axis.
+     * @return true if the event is consumed, else false
+     */
+    @Override
+    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+        gesturesLogTextView.append("\nonFling: velocityX is " + velocityX + ", velocityY is " + velocityY);
+        if(Math.abs(velocityX) > Math.abs(velocityY)){
+            if(velocityX < 0){
+                gesturesLogTextView.append("\nYou swiped left");
+            } else{
+                gesturesLogTextView.append("\nYou swiped right");
+            }
+        } else{
+            if(velocityY < 0){
+                gesturesLogTextView.append("\nYou swiped up");
+            } else{
+                gesturesLogTextView.append("\nYou swiped down");
+            }
+        }
+        flings++;
+        flingTextView.setText(flings + "");
+        return true;
+    }
+
+    public void clearAll(View v){
+        gesturesLogTextView.setText("");
+
+        singleTaps = 0;
+        longPresses = 0;
+        doubleTaps = 0;
+        scrolls = 0;
+        flings = 0;
+
+        singleTapTextView.setText("0");
+        longPressTextView.setText("0");
+        doubleTapTextView.setText("0");
+        scrollTextView.setText("0");
+        flingTextView.setText("0");
+    }
 }
